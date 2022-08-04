abdd35bd01a50eb18e7a758289a93d9ee16433e5
Kujou Yuko <i@779.moe>
2022-08-04 01:35:52 +0000

    Settings: Add status bar lyric [2/2]
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I944afb9e961eee72ce7fd183f2a97a9210f6c35a

---
+++ src/org/lineageos/lineageparts/statusbar/StatusbarLyricSettings.java
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Project Kaleidoscope
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.lineageos.lineageparts.statusbar;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import org.lineageos.lineageparts.R;
+import org.lineageos.lineageparts.SettingsPreferenceFragment;
+
+public class StatusbarLyricSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.status_bar_lyric_settings);
+        getActivity().setTitle(R.string.status_bar_lyric_title);
+    }
+}
