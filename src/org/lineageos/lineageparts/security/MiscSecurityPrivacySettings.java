3995aaf9f89ab8ce9e914a8f5b98a0a233f87737
someone5678 <someone5678@users.noreply.github.com>
2024-06-01 16:47:02 +0900

    Settings: Add additional security settings preference
    Change-Id: I84f4e323fb381c416d269ddf8c0bcd702b5e722c

---
+++ src/org/lineageos/lineageparts/security/MiscSecurityPrivacySettings.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2024 TheParasiteProject
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.lineageparts.security;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.ArraySet;
+
+import org.lineageos.lineageparts.R;
+import org.lineageos.lineageparts.SettingsPreferenceFragment;
+import org.lineageos.lineageparts.search.BaseSearchIndexProvider;
+import org.lineageos.lineageparts.search.Searchable;
+
+import java.util.List;
+import java.util.Set;
+
+public class MiscSecurityPrivacySettings extends SettingsPreferenceFragment implements Searchable  {
+
+    public static final String TAG = "MiscSecurityPrivacySettings";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.misc_security_privacy_settings);
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public Set<String> getNonIndexableKeys(Context context) {
+                    final Set<String> result = new ArraySet<>();
+                    return result;
+                }
+            };
+}
