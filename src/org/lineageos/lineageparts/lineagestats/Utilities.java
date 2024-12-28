9d78f316a4f24f55461cf6bc9d6f83db3a87d512
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-28 09:24:33 +0900

    LineageParts: Parasitizing
    Change-Id: I6e5c4ec864157b8fdcab833f69f0d9ff273ab205

--- src/org/lineageos/lineageparts/lineagestats/Utilities.java
+++ src/org/lineageos/lineageparts/lineagestats/Utilities.java
@@ -64,11 +64,11 @@ public class Utilities {
     }
 
     public static String getDevice() {
-        return SystemProperties.get("ro.lineage.device", Build.PRODUCT);
+        return SystemProperties.get("ro.custom.device", Build.PRODUCT);
     }
 
     public static String getModVersion() {
-        return SystemProperties.get("ro.lineage.version", Build.DISPLAY);
+        return SystemProperties.get("ro.custom.version", Build.DISPLAY);
     }
 
     public static String digest(String input) {
